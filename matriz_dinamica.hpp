8a00470988163e173990c66c4d69c8f0ba11b864
Matheus Henrique <85768227+Mathteus@users.noreply.github.com>
2021-11-01 14:24:08 -0300

    Add files via upload

--- matriz_dinamica.hpp
+++ matriz_dinamica.hpp
@@ -15,88 +15,10 @@
 #include "sqlite3/sqlite3.h"
 #include "MpScQueue.h"
 #include "sthread.hpp"
+#include "Bancodados.hpp"
 using namespace std;
 using namespace chrono;
 typedef unsigned int unit;
-
-class BancoDados{
-private:
-    static int callback(void *data, int argc, char **argv, char **azColName){
-        //fprintf(stderr, "%s: ", (const char*)data);
-        for (int i = 0; i < argc; i++){
-            printf("%s \t %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-        }
-        printf("\n");
-        return 0;
-    }
-
-    atomic<sqlite3*> DB;
-    bool resposta;
-    string erro, comando; 
-    char* mensagem_erro;
-    mutex mute;
-
-public:
-    atomic<unit> acessados;
-    BancoDados(const char* name) : resposta(false), erro(""), comando(""), acessados(0) {
-        resposta = sqlite3_open(name, reinterpret_cast<sqlite3**>(&DB));
-        if (resposta != SQLITE_OK){
-            cerr << "erro: [" << sqlite3_errmsg(DB) << "]\n"
-                 << endl;
-            sqlite3_free(mensagem_erro);
-            exit(-1);
-        }
-    }
-
-    ~BancoDados(){
-        sqlite3_close(DB);
-        sqlite3_free(mensagem_erro);
-        exit(-1);
-    }
-
-    void inserir(string arr, string arr2, bool possui){
-        lock_guard<mutex> lguard(mute);
-        //unit porcetagem = acessados * 100 / 5000;
-        //cout << "carregando " << porcetagem << "%\n";
-        comando = "insert into dinamica (matriz, vetor, possui) values ('" + arr + "' , '" + arr2 + "', '" + to_string(possui) + "');";
-        resposta = sqlite3_exec(DB, comando.c_str(), NULL, 0, &mensagem_erro);
-        erro = sqlite3_errmsg(DB);
-
-        if (erro == "no such table: dinamica"){
-            comando = "create table dinamica (id integer not null primary key autoincrement, matriz text not null, vetor text not null, possui bool not null);";
-            resposta = sqlite3_exec(DB, comando.c_str(), NULL, 0, &mensagem_erro);
-            comando = "insert into dinamica (matriz, vetor, possui) values ('" + arr + "' , '" + arr2 + "', '" + to_string(possui) + "');";
-            resposta = sqlite3_exec(DB, comando.c_str(), NULL, 0, &mensagem_erro);
-        }
-
-        if (resposta == SQLITE_BUSY){
-            cerr << "erro: [nao foi possui continuar a operacao multiplos threds usando a mesma abertura]\n";
-            exit(-1);
-        }
-
-        if (resposta != SQLITE_OK){
-            cerr << "erro: [" << sqlite3_errmsg(DB) << "]\n"
-                 << endl;
-            exit(-1);
-        }
-
-        //acessados++;
-    }
-
-    void select(){
-        lock_guard<mutex> lguard(mute);
-        comando = "select * from dinamica;";
-        resposta = sqlite3_exec(DB, comando.c_str(), callback, 0, &mensagem_erro);
-        erro = sqlite3_errmsg(DB);
-
-        if (resposta != SQLITE_OK){
-            cerr << "erro: [" << sqlite3_errmsg(DB) << "]\n"
-                 << endl;
-            sqlite3_free(mensagem_erro);
-            exit(-1);
-        }
-    }
-};
 
 inline int randomico(int maximo, int minimo){
     return (rand() % maximo + minimo);
